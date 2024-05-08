987c26629e142ba2b195017284aea40403f7a70e
Christian Hanisch <info@tandron.de>
2024-05-08 19:56:59 +0200

    Refactoring

--- CH_PurchaseWpfModule/ViewModels/PurchaseMainViewModel.cs
+++ CH_PurchaseWpfModule/ViewModels/PurchaseMainViewModel.cs
@@ -22,7 +22,7 @@ namespace CH_PurchaseWpfModule.ViewModels
         {
             eventAggregator.GetEvent<ErpModeMessageSentEvent>().Subscribe(MessageReceived);
             LoadFromDatabase();
-            ImportCompanyPurchases();
+            //ImportCompanyPurchases();
         }
 
         private void MessageReceived(ErpMode mode)
